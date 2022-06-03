c6a2fdb3a5f801e8caee693d4d8cc38d8e1ef10f
Venita <nube1124@yahoo.com>
2022-06-02 21:40:46 -0400

    Initial Upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationbarComponent } from './navigationbar/navigationbar.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { SponsorComponent } from './sponsor/sponsor.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationbarComponent,
+    WelcomeComponent,
+    CarouselComponent,
+    SponsorComponent,
+    PaymentComponent,
   ],
   imports: [
     BrowserModule,
